1fae77fda46882fdc42999be9b0933431628f14a
aimdexter <draimade@gmail.com>
2023-05-19 17:19:46 +0100

    fix: fixed signin for single offer and offers

--- src/providers/generateStaticParamsProvider.js
+++ src/providers/generateStaticParamsProvider.js
@@ -1,12 +1,13 @@
 'use client'
 
+import SignInButton from '@/components/common/SignInButton'
 import { useSession } from 'next-auth/react'
 
 function GenerateStaticParamsProvider({ children }) {
   const { data: session, status } = useSession()
   if (status === 'authenticated') {
     return <div>{children}</div>
-  }
+  } else return <SignInButton />
 }
 
 export default GenerateStaticParamsProvider
