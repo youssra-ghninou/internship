833c39dcba5f54d3e102b0ca504363857edf0edd
ussraa <92607242+youssra-ghninou@users.noreply.github.com>
2023-05-21 20:18:39 +0100

    feat: profile page done

--- src/components/profile/MyProfile.jsx
+++ src/components/profile/MyProfile.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
+import EditButton from './EditButton'
 
 export default function Profile({ data }) {
   return (
-    <div className=''>
-      <div className='flex flex-col items-center'>
-        <div className='flex items-center gap-2'>
+    <div className=' text-left '>
+      <div className=' text-[28px] font-semibold '>Mon profil</div>
+      <div className='flex flex-col items-center gap-5'>
+        <div className='flex w-full items-center gap-2 rounded-xl border-2 p-3 shadow-md'>
           <Image
             className='rounded-full'
             priority
@@ -13,104 +15,142 @@ export default function Profile({ data }) {
             src={data.image.replaceAll('s96-c', 's400-c')}
             alt={`${data.name}'s profile picture`}
           />
-          <div className=''>
+          <div className='flex flex-col gap-1'>
             <div className='name font-bold'>{data.name}</div>
-            <div className='email text-gray-500'>{data.email}</div>
+            <div className='name font-thin '>{data.profile.titre}</div>
           </div>
         </div>
-        <div className='Profile info'>
-          <div className='adresse mt-2 text-gray-500'>
-            <span className='font-medium'>Adresse : </span>{' '}
-            {data.profile.adresse}
+        <div className='flex w-full flex-col gap-4 rounded-xl border-2 p-5 shadow-md'>
+          <div className='flex items-center justify-between'>
+            <div className='name font-bold'>Informations personnelles :</div>
+            <EditButton />
           </div>
-          <div className='titre mt-2 text-gray-500'>
-            <span className='font-medium'>Titre : </span> {data.profile.titre}
+          <div className='flex'>
+            <div className='flex w-full flex-col pl-5'>
+              <div className='titre mt-2 text-gray-500'>
+                <span className='font-medium'>Nom complet : </span> {data.name}
+              </div>
+              <div className='telephone mt-2 text-gray-500'>
+                <span className='font-medium'>Téléphone : </span>
+                {data.profile.telephone}
+              </div>
+            </div>
+            <div className='flex w-full flex-col pl-5'>
+              <div className='titre mt-2 text-gray-500'>
+                <span className='font-medium'>Titre : </span>{' '}
+                {data.profile.titre}
+              </div>
+
+              <div className='adresse mt-2 text-gray-500'>
+                <span className='font-medium'>Adresse Email : </span>{' '}
+                {data.email}
+              </div>
+            </div>
           </div>
-          <div className='telephone mt-2 text-gray-500'>
-            <span className='font-medium'>Téléphone : </span>
-            {data.profile.telephone}
-          </div>
-          <div className='site-web mt-2 text-gray-500'>
-            <span className='font-medium'>Portfolio : </span>
-            {data.profile.siteWeb}
-          </div>
-          <div className='resume mt-2 text-gray-500'>
+          <div className='resume mt-2 pl-5 text-gray-500'>
             <span className='font-medium'>Bio : </span> {data.profile.resume}
           </div>
-          <div className='education mt-2 flex items-center gap-5 text-gray-500'>
-            <span className='font-medium'>Éducation : </span>
-            {data.profile.education.map((educ) => {
-              return (
-                <div key={educ.id} className='mt-2'>
-                  <div className='etablissement'>
-                    <span className='font-medium'>Etablissement : </span>
-                    {educ.etablissement}
-                  </div>
-                  <div className='diplome text-gray-500'>
-                    <span className='font-medium'>Diplôme : </span>{' '}
-                    {educ.diplome}
-                  </div>
-                  <div className='domaine text-gray-500'>
-                    <span className='font-medium'>Domaine des études : </span>
-                    {educ.domaine}
-                  </div>
-                  <div className='date-debut text-gray-500'>
-                    <span className='font-medium'>
-                      Date de début de formation :
-                    </span>
-                    {educ.dateDebut}
-                  </div>
-                  <div className='date-fin text-gray-500'>
-                    <span className='font-medium'>
-                      Date d{"'"}obtention du diplôme :
-                    </span>
-                    {educ.dateFin}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-          <div className='experience mt-2 flex items-center gap-5 text-gray-500'>
-            <span className='font-medium'>Experience : </span>
-            {data.profile.experience.map((exp) => {
-              return (
-                <div key={exp.id} className='mt-2'>
-                  <div className='etablissement'>
-                    <span className='font-medium'>Etablissement : </span>
-                    {exp.entreprise}
-                  </div>
-                  <div className='diplome text-gray-500'>
-                    <span className='font-medium'>Role : </span> {exp.poste}
-                  </div>
-                  <div className='domaine text-gray-500'>
-                    <span className='font-medium'>Description du poste : </span>
-                    {exp.description}
-                  </div>
-                  <div className='date-debut text-gray-500'>
-                    <span className='font-medium'>Date de début : </span>
-                    {exp.dateDebut}
-                  </div>
-                  <div className='date-fin text-gray-500'>
-                    <span className='font-medium'>Date fin : </span>
-                    {exp.dateFin ? exp.date : 'Travaille actuelle'}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-          <div className='education mt-2 flex items-center gap-5 text-gray-500'>
-            <span className='font-medium'>Competences :</span>
-            {data.profile.competences.map((comp) => {
-              return (
-                <div key={comp.id} className='mt-2'>
-                  <div className='cursor-default rounded-md border-2 px-2 py-1 text-xs font-bold text-black transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-blue-500 hover:to-yellow-500'>
-                    {comp.nom}
-                  </div>
-                </div>
-              )
-            })}
-          </div>
         </div>
+        <div className='flex w-full flex-col gap-4 rounded-xl border-2 p-5 shadow-md'>
+          <div className='flex items-center justify-between'>
+            <span className='font-bold'>Éducation : </span>
+            <EditButton />
+          </div>
+          {data.profile.education.map((educ) => {
+            return (
+              <div key={educ.id} className='mt-2'>
+                <div className='etablissement'>
+                  <span className='font-medium'>Etablissement : </span>
+                  {educ.etablissement}
+                </div>
+                <div className='flex'>
+                  <div className='flex w-full flex-col pl-5'>
+                    <div className='diplome text-gray-500'>
+                      <span className='font-medium'>Diplôme : </span>{' '}
+                      {educ.diplome}
+                    </div>
+                    <div className='date-debut text-gray-500'>
+                      <span className='font-medium'>
+                        Date de début de formation :
+                      </span>
+                      {educ.dateDebut}
+                    </div>
+                  </div>
+                  <div className='flex w-full flex-col pl-5'>
+                    <div className='domaine text-gray-500'>
+                      <span className='font-medium'>Domaine des études : </span>
+                      {educ.domaine}
+                    </div>
+                    <div className='date-fin text-gray-500'>
+                      <span className='font-medium'>
+                        Date d{"'"}obtention du diplôme :
+                      </span>
+                      {educ.dateFin}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className='flex w-full flex-col gap-4 rounded-xl border-2 p-3 shadow-md'>
+          <div className='flex items-center justify-between'>
+            <span className='font-bold'>Éxpériences : </span>
+            <EditButton />
+          </div>
+          {data.profile.experience.map((exp) => {
+            return (
+              <div key={exp.id} className='mt-2'>
+                <div className='etablissement'>
+                  <span className='font-medium'>Etablissement : </span>
+                  {exp.entreprise}
+                </div>
+                <div className='flex'>
+                  <div className='flex w-full flex-col pl-5'>
+                    <div className='diplome text-gray-500'>
+                      <span className='font-medium'>Role : </span> {exp.poste}
+                    </div>
+                    <div className='date-debut text-gray-500'>
+                      <span className='font-medium'>Date de début : </span>
+                      {exp.dateDebut}
+                    </div>
+                  </div>
+                  <div className='flex w-full flex-col pl-5'>
+                    <div className='domaine text-gray-500'>
+                      <span className='font-medium'>
+                        Description du poste :{' '}
+                      </span>
+                      {exp.description}
+                    </div>
+
+                    <div className='date-fin text-gray-500'>
+                      <span className='font-medium'>Date fin : </span>
+                      {exp.dateFin ? exp.dateFin : 'Travaille actuellement'}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className='flex w-full items-center justify-between gap-4 rounded-xl border-2 p-3 shadow-md'>
+          <div className='flex items-center gap-3 '>
+            <span className='font-bold'>Competences </span>
+            <div className='flex flex-wrap'>
+              {data.profile.competences.map((comp) => {
+                return (
+                  <div key={comp.id} className='mt-2 flex'>
+                    <div className='cursor-default rounded-md border-2 px-2 py-1 text-xs font-bold text-black transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gradient-to-r hover:from-blue-500 hover:to-yellow-500'>
+                      {comp.nom}
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+
+          <EditButton />
+        </div>{' '}
       </div>
     </div>
   )
