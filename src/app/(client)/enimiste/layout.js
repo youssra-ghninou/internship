8f3446421c90ed6fc70e4d95f809c99b9e964ac6
aimdexter <draimade@gmail.com>
2023-05-21 20:19:19 +0100

    feat: added layout to everything

--- src/app/(client)/enimiste/layout.js
+++ src/app/(client)/enimiste/layout.js
@@ -1,6 +1,4 @@
 import SignInButton from '@/components/common/SignInButton'
-import Side from '@/components/sideBar/side'
-import Top from '@/components/topbar/top'
 import { authOptions } from '@/pages/api/auth/[...nextauth]'
 import { getUser } from '@@/queries'
 import { getServerSession } from 'next-auth'
@@ -11,16 +9,7 @@ export default async function EnimisteLayout({ children }) {
   if (session) {
     const user = await getUser(session.user.email)
     if (user.role === 'ENIMISTE') {
-      return (
-        <div>
-          <Top Nom={user.name} lien={user.image} />
-
-          <div className='flex w-screen'>
-            <Side />
-            <div className='w-full px-5 py-5'>{children}</div>
-          </div>
-        </div>
-      )
+      return <>{children}</>
     } else return redirect('/' + user.role.toLowerCase())
   }
   return (
--- src/components/sideBar/side.jsx
+++ src/components/sideBar/side.jsx
@@ -1,13 +1,17 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import SignOutButton from '../common/SignOutButton'
 
-const Side = ({ eventnotif }) => {
+const Side = (board) => {
   return (
     <div className='sticky top-5 bottom-0 left-0 flex h-screen w-fit flex-col items-center justify-center gap-10 bg-[#22506C] text-xs font-normal'>
       <div className='flex flex-row justify-between'>
         <p className='h-[55px] w-[7px] rounded-r-lg bg-[#FDDC30] '></p>
-        <button className='font-popping flex flex-col items-center text-white '>
+        <Link
+          className='font-popping flex flex-col items-center text-center text-white hover:underline '
+          href={board}
+        >
           <Image
             width={20}
             height={20}
@@ -16,34 +20,43 @@ const Side = ({ eventnotif }) => {
             className='pb-1'
           />
           Tableau De Bord
-        </button>
+        </Link>
         <p className='h-[55px] w-[7px] rounded-l-lg bg-[#FDDC30] '></p>
       </div>
-      <div className='flex flex-col items-center justify-center'>
-        <Image
-          width={20}
-          height={20}
-          src={'/calendrier.png'}
-          alt={'offres'}
-          className='pb-1'
-        />
+      <div className='flex flex-row justify-between'>
+        <p className='h-[55px] w-[7px] rounded-r-lg bg-[#FDDC30] '></p>
         <Link
-          className='font-popping flex flex-col items-center text-center text-white '
-          href='offers'
+          className='font-popping flex flex-col items-center text-center text-white hover:underline'
+          href={'/offers'}
         >
-          Mes offres
+          <Image
+            width={20}
+            height={20}
+            src={'/calendrier.png'}
+            alt={'offres'}
+            className='pb-1'
+          />
+          Offres de stage
         </Link>
+        <p className='h-[55px] w-[7px] rounded-l-lg bg-[#FDDC30] '></p>
       </div>
-      <button className='font-popping flex flex-col items-center text-white '>
-        <Image
-          width={20}
-          height={20}
-          src={'/parametres.png'}
-          alt={'parametre'}
-          className='pb-1'
-        />
-        Paramètre
-      </button>
+      <div className='flex flex-row justify-between'>
+        <p className='h-[55px] w-[7px] rounded-r-lg bg-[#FDDC30] '></p>
+        <Link
+          className='font-popping flex flex-col items-center text-center text-white hover:underline'
+          href={board}
+        >
+          <Image
+            width={20}
+            height={20}
+            src={'/parametres.png'}
+            alt={'parametre'}
+            className='pb-1'
+          />
+          Paramètre
+        </Link>
+        <p className='h-[55px] w-[7px] rounded-l-lg bg-[#FDDC30] '></p>
+      </div>
       <SignOutButton />
     </div>
   )
