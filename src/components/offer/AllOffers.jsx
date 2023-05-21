9d94484a6d86722c41f37b1987012fdd1e2c5d91
aimdexter <draimade@gmail.com>
2023-05-22 00:11:01 +0100

    fix: deleted slice() methode

--- src/components/offer/AllOffers.jsx
+++ src/components/offer/AllOffers.jsx
@@ -14,54 +14,50 @@ export default async function AllOffers() {
           Voir Tout
         </a>
       </div>
-      {offers
-        .slice(0, 4)
-        .map(
-          ({
-            id,
-            title,
-            description,
-            status,
-            startDate,
-            endDate,
-            author,
-            company,
-          }) => (
-            <>
-              <div
-                key={id}
-                className='flex w-full items-center justify-between gap-36 rounded-xl bg-white px-5 py-5 shadow-xl'
-              >
-                <div className='flex items-center gap-3  '>
-                  <Image
-                    width={100.03}
-                    height={60.09}
-                    src={company.image}
-                    className='pt-[14.78px]'
-                    alt={company.name}
-                  />
-                  <div className='font-popping flex flex-col gap-2 font-bold'>
-                    <div className='pt-[10.32px]'>{title}</div>
-                    <div className='pb-[1px] text-xs text-gray-400 '>
-                      {company.name}
-                    </div>
-                    <div className='text-sm text-gray-400'>
-                      {'Rabat, Maroc'}
-                    </div>
+      {offers.map(
+        ({
+          id,
+          title,
+          description,
+          status,
+          startDate,
+          endDate,
+          author,
+          company,
+        }) => (
+          <>
+            <div
+              key={id}
+              className='flex w-full items-center justify-between gap-36 rounded-xl bg-white px-5 py-5 shadow-xl'
+            >
+              <div className='flex items-center gap-3  '>
+                <Image
+                  width={100.03}
+                  height={60.09}
+                  src={company.image}
+                  className='pt-[14.78px]'
+                  alt={company.name}
+                />
+                <div className='font-popping flex flex-col gap-2 font-bold'>
+                  <div className='pt-[10.32px]'>{title}</div>
+                  <div className='pb-[1px] text-xs text-gray-400 '>
+                    {company.name}
                   </div>
+                  <div className='text-sm text-gray-400'>{'Rabat, Maroc'}</div>
                 </div>
-                <Remuneration prix={'900dh'} />
-                <Button nomButton={'status'} />
-                <Link
-                  className='rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700'
-                  href={'/offers/' + id}
-                >
-                  Details
-                </Link>
               </div>
-            </>
-          ),
-        )}
+              <Remuneration prix={'900dh'} />
+              <Button nomButton={'status'} />
+              <Link
+                className='rounded-md bg-blue-500 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700'
+                href={'/offers/' + id}
+              >
+                Details
+              </Link>
+            </div>
+          </>
+        ),
+      )}
     </div>
   )
 }
