3d62667b272fb5bfc407b3e1e8185a8e718520d8
aimdexter <draimade@gmail.com>
2023-06-10 02:51:41 +0100

    chore: fixing push error

--- src/app/offers/[id]/page.js
+++ src/app/offers/[id]/page.js
@@ -1,25 +1,25 @@
-// import SingleOffer from '@/components/offer/SingleOffer'
-// import CreateProfile from '@/components/profile/CreateProfile'
-// import { authOptions } from '@/pages/api/auth/[...nextauth]'
-// import { getOffer, getOffers, getUser } from '@@/queries'
-// import { getServerSession } from 'next-auth'
+import SingleOffer from '@/components/offer/SingleOffer'
+import CreateProfile from '@/components/profile/CreateProfile'
+import { authOptions } from '@/pages/api/auth/[...nextauth]'
+import { getOffer, getOffers, getUser } from '@@/queries'
+import { getServerSession } from 'next-auth'
 
-// export default async function OfferDetails({ params }) {
-//   const { id } = params
-//   const offer = await getOffer(id)
-//   const session = await getServerSession(authOptions)
-//   if (session) {
-//     const user = await getUser(session.user.email)
-//     if (user.profile) {
-//       return <SingleOffer offer={offer} />
-//     }
-//     return <CreateProfile />
-//   }
-// }
+export default async function OfferDetails({ params }) {
+  const { id } = params
+  const offer = await getOffer(id)
+  const session = await getServerSession(authOptions)
+  if (session) {
+    const user = await getUser(session.user.email)
+    if (user.profile) {
+      return <SingleOffer offer={offer} />
+    }
+    return <CreateProfile />
+  }
+}
 
-// export async function generateStaticParams() {
-//   const offers = await getOffers()
-//   return offers.map((offer) => ({
-//     id: offer.id,
-//   }))
-// }
+export async function generateStaticParams() {
+  const offers = await getOffers()
+  return offers.map((offer) => ({
+    id: offer.id,
+  }))
+}
